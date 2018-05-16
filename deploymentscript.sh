ed63d5d6a0941f6fceb2d5c6f8052b4e44c144b2
karmya <39350265+karmya@users.noreply.github.com>
2018-05-16 14:43:12 -0700

    Update deploymentscript.sh

--- deploymentscript.sh
+++ deploymentscript.sh
@@ -6,16 +6,24 @@ cid=--compartment-id
 #set avalability domain i call it av_domain
 av_domain=--availability-domain
 
-##set tenant name
-tenant=ocid1.tenancy.oc1..aaaaaaaax3q5jcmovrv7qxbops55wzrjeutinobtayaykdasoygtnpko7buq
+##set tenant/compartment name
+C=ocid1.tenancy.oc1..aaaaaaaax3q5jcmovrv7qxbops55wzrjeutinobtayaykdasoygtnpko7buq
 
 ##set OS image, it is defined per region, 
 OS=ocid1.image.oc1.phx.aaaaaaaav4gjc4l232wx5g5drypbuiu375lemgdgnc7zg2wrdfmmtbtyrc5q #OracleLinux, PHX
 
 ##set region it is always named with the following convention EU/US-phx/ashburn/frankfurt-1
-reguon=US-PHX-1
+region=US-PHOENIX-1
 
 ##set avalability domain i call it ad
 ad=kWVD:PHX-AD-1
 
-oci compute instance list $CID $tenant $av_domain $ad  --display-name tejas-cli
+##create a virtual network 
+echo "creating virtual network"
+
+vent=`oci network vcn create --region $region --cidr-block 10.0.0.0/24 $cid $C --display-name "hpc_vnet" --wait-for-state AVAILABLE | jq -r '.data.id'`
+
+
+#oci compute instance list $cid $C $av_domain $ad --display-name tejas-cli
+~                                                                                                                                                                                  
+~                                                                             
